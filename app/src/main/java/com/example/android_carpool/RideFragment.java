ec9bba52205821de53378b0a781a23b7ca5bca82
EdwardKHKim <kim.kyoohoon.edward@gmail.com>
2020-07-12 22:16:40 -0700

    Add images

--- app/src/main/java/com/example/android_carpool/RideFragment.java
+++ app/src/main/java/com/example/android_carpool/RideFragment.java
@@ -11,9 +11,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-
-import java.util.Objects;
-
 public class RideFragment extends Fragment {
 
     private View view;
